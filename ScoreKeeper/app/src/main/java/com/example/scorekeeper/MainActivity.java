cd1464e74df6df8a3684ce5d2434eac1adf3302d
Younes Charfaoui <mxcsyounes@gmail.com>
2018-12-11 16:29:56 +0100

    Making The layout, need some polish and reset button

--- ScoreKeeper/app/src/main/java/com/example/scorekeeper/MainActivity.java
+++ ScoreKeeper/app/src/main/java/com/example/scorekeeper/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.scorekeeper;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.team_a_corner_button).setOnClickListener(this);
+        findViewById(R.id.team_b_corner_button).setOnClickListener(this);
+        findViewById(R.id.team_a_foul_button).setOnClickListener(this);
+        findViewById(R.id.team_b_foul_button).setOnClickListener(this);
+        findViewById(R.id.team_a_goal_button).setOnClickListener(this);
+        findViewById(R.id.team_b_goal_button).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+
+        }
     }
 }
