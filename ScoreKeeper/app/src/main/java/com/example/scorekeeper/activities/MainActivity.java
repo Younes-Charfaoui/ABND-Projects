4b56896418215602b076d806ccc0c50b70c2f4f6
Younes Charfaoui <mxcsyounes@gmail.com>
2018-12-12 12:02:16 +0100

    Refactoring and Commenting the code in the MainActivity Class

--- ScoreKeeper/app/src/main/java/com/example/scorekeeper/activities/MainActivity.java
+++ ScoreKeeper/app/src/main/java/com/example/scorekeeper/activities/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.scorekeeper;
+package com.example.scorekeeper.activities;
 
 import android.content.DialogInterface;
 import android.os.Bundle;
@@ -7,11 +7,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+import com.example.scorekeeper.R;
 
+/**
+ * We choose the Soccer Sport to make a Score Keeper App the holds some metrics
+ * for both teams : A and B, the metrics are the Goals, Fouls and Corners, each one is
+ * incremented by one.
+ */
+public class MainActivity extends AppCompatActivity implements View.OnClickListener,
+        DialogInterface.OnClickListener {
+
+    // integers variables to hold the current metrics of the game.
     private int aTeamGoals = 0, bTeamGoals = 0, aTeamFouls = 0,
             bTeamFouls = 0, aTeamCorners = 0, bTeamCorners = 0;
 
+    // textViews variables to reference the textViews.
     private TextView aGoalsTv, bGoalsTv, aFoulsTv,
             bFoulsTv, aCornersTv, bCornersTv;
 
@@ -36,33 +46,32 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         bGoalsTv = findViewById(R.id.team_b_goals_tv);
     }
 
+    /**
+     * The overridden method by the ClickListener Interface to handle view click.
+     *
+     * @param viewClicked tells which view was clicked.
+     */
     @Override
     public void onClick(View viewClicked) {
         findViewById(R.id.reset_button).setEnabled(true);
         switch (viewClicked.getId()) {
             case R.id.team_a_corner_button:
-                aTeamCorners++;
-                changeTextView(aCornersTv, aTeamCorners);
+                changeTextView(aCornersTv, ++aTeamCorners);
                 break;
             case R.id.team_b_corner_button:
-                bTeamCorners++;
-                changeTextView(bCornersTv, bTeamCorners);
+                changeTextView(bCornersTv, ++bTeamCorners);
                 break;
             case R.id.team_a_foul_button:
-                aTeamFouls++;
-                changeTextView(aFoulsTv, aTeamFouls);
+                changeTextView(aFoulsTv, ++aTeamFouls);
                 break;
             case R.id.team_b_foul_button:
-                bTeamFouls++;
-                changeTextView(bFoulsTv, bTeamFouls);
+                changeTextView(bFoulsTv, ++bTeamFouls);
                 break;
             case R.id.team_a_goal_button:
-                aTeamGoals++;
-                changeTextView(aGoalsTv, aTeamGoals);
+                changeTextView(aGoalsTv, ++aTeamGoals);
                 break;
             case R.id.team_b_goal_button:
-                bTeamGoals++;
-                changeTextView(bGoalsTv, bTeamGoals);
+                changeTextView(bGoalsTv, ++bTeamGoals);
                 break;
             case R.id.reset_button:
                 reset();
@@ -70,31 +79,34 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     /**
-     * This Method reset the variables and the text views to the 0.
+     * This Method displays dialog to reset or not the variables and the
+     * text views to the 0.
      */
     private void reset() {
         new AlertDialog.Builder(this)
                 .setTitle(R.string.reset)
-                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        aTeamGoals = 0;
-                        bTeamGoals = 0;
-                        aTeamFouls = 0;
-                        bTeamFouls = 0;
-                        aTeamCorners = 0;
-                        bTeamCorners = 0;
-                        changeTextView(aCornersTv, 0);
-                        changeTextView(bCornersTv, 0);
-                        changeTextView(aFoulsTv, 0);
-                        changeTextView(bFoulsTv, 0);
-                        changeTextView(aGoalsTv, 0);
-                        changeTextView(bGoalsTv, 0);
-                        findViewById(R.id.reset_button).setEnabled(false);
-                    }
-                })
+                .setPositiveButton(android.R.string.ok, this)
                 .setNegativeButton(R.string.cancel, null)
                 .setMessage(R.string.reset_message).show();
+    }
+
+    /**
+     * this method make the necessary action to reset the variable and the texts.
+     */
+    private void resetProcedure() {
+        aTeamGoals = 0;
+        bTeamGoals = 0;
+        aTeamFouls = 0;
+        bTeamFouls = 0;
+        aTeamCorners = 0;
+        bTeamCorners = 0;
+        changeTextView(aCornersTv, 0);
+        changeTextView(bCornersTv, 0);
+        changeTextView(aFoulsTv, 0);
+        changeTextView(bFoulsTv, 0);
+        changeTextView(aGoalsTv, 0);
+        changeTextView(bGoalsTv, 0);
+        findViewById(R.id.reset_button).setEnabled(false);
     }
 
     /**
@@ -104,6 +116,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
      * @param value    the value which we want to display in the TextView.
      */
     private void changeTextView(TextView textView, int value) {
-        textView.setText(String.valueOf(value));
+        /*
+         * we cant let user update a textView by more than 1000, because I don't think that we
+         * can pass this number in real soccer game. I think 100 is better but I choose 1000.
+         */
+        if (value <= 1000)
+            textView.setText(String.valueOf(value));
+    }
+
+    /**
+     * The Overridden method by the Dialog Interface to handing clicks.
+     *
+     * @param dialog which dialog the click was on.
+     * @param which  button was pressed.
+     */
+    @Override
+    public void onClick(DialogInterface dialog, int which) {
+        if (which == DialogInterface.BUTTON_POSITIVE)
+            resetProcedure();
     }
 }
