47b18605d48a42d9cce503964da16eecf43d5120
Younes Charfaoui <mxcsyounes@gmail.com>
2018-12-17 00:33:59 +0100

    Adding new Activity and new drawables

---
+++ MusicalStructure/app/src/main/java/com/example/musicalstructure/activities/NowPlayingActivity.java
@@ -0,0 +1,22 @@
+package com.example.musicalstructure.activities;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.example.musicalstructure.R;
+
+public class NowPlayingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_now_playing);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+}
