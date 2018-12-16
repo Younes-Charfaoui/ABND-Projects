d06715077c14c152cec7c676ba54a334ab851076
Younes Charfaoui <mxcsyounes@gmail.com>
2018-12-16 13:12:41 +0100

    Setting listeners and adapting layout

--- MusicalStructure/app/src/main/java/com/example/musicalstructure/MainActivity.java
+++ MusicalStructure/app/src/main/java/com/example/musicalstructure/MainActivity.java
@@ -1,13 +1,34 @@
 package com.example.musicalstructure;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Initializing The ClickListeners.
+        findViewById(R.id.album_card).setOnClickListener(this);
+        findViewById(R.id.artist_card).setOnClickListener(this);
+        findViewById(R.id.genre_card).setOnClickListener(this);
+        findViewById(R.id.playlist_card).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.album_card:
+                break;
+            case R.id.artist_card:
+                break;
+            case R.id.genre_card:
+                break;
+            case R.id.playlist_card:
+                break;
+        }
+
     }
 }
