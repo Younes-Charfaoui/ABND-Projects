fca5d6286f2389ea20d679721aaaf7fe90f8a0e9
Younes Charfaoui <mxcsyounes@gmail.com>
2018-12-16 23:59:45 +0100

    Adding List to DataProvider

---
+++ MusicalStructure/app/src/main/java/com/example/musicalstructure/utils/DataProvider.java
@@ -0,0 +1,36 @@
+package com.example.musicalstructure.utils;
+
+import com.example.musicalstructure.activities.MusicActivity;
+import com.example.musicalstructure.models.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DataProvider {
+
+    private static List<Data> albumList;
+    private static List<Data> artistList;
+    private static List<Data> genreList;
+    private static List<Data> playList;
+
+    static {
+        albumList = new ArrayList<>();
+        artistList = new ArrayList<>();
+        genreList = new ArrayList<>();
+        playList = new ArrayList<>();
+    }
+
+    public static List<Data> getData(int type) {
+        switch (type) {
+            case MusicActivity.TYPE_ARTIST:
+                return artistList;
+            case MusicActivity.TYPE_PLAYLIST:
+                return playList;
+            case MusicActivity.TYPE_GENRE:
+                return genreList;
+            case MusicActivity.TYPE_ALBUM:
+                return albumList;
+        }
+
+    }
+}
