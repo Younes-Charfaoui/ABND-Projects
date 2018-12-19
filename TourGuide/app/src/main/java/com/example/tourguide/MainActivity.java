7570d6fbeae1a9fa823df441cd81a7cc171f6989
Younes Charfaoui <mxcsyounes@gmail.com>
2018-12-19 10:30:34 +0100

    Reseting Gradle, making another activity

--- TourGuide/app/src/main/java/com/example/tourguide/MainActivity.java
+++ TourGuide/app/src/main/java/com/example/tourguide/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.tourguide;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
+
 }
