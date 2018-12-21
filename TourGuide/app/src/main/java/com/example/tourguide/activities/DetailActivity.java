e94fef1b8a2e2759dcb3c946147a8e1619a52e64
Younes Charfaoui <mxcsyounes@gmail.com>
2018-12-21 16:40:12 +0100

    Adding icons, and design

--- TourGuide/app/src/main/java/com/example/tourguide/activities/DetailActivity.java
+++ TourGuide/app/src/main/java/com/example/tourguide/activities/DetailActivity.java
@@ -15,7 +15,7 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
